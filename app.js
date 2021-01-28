e3aaac6a633204e2185bacc5f032940f2c65fb4a
Jonathan Smith <jpls218@gmail.com>
2021-01-27 20:41:08 -0500

    deleted index.js and added more functions

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const inquirer = require('inquirer');
 const logo = require('asciiart-logo');
-// const db = require('./db');
+const { start } = require('repl');
+const db = require('./db');
+const connection = require('./db/connection');
 require("console.table");
 
 
@@ -8,43 +10,78 @@ require("console.table");
 function init() {
     const logoText = logo({ name: "Jonathan's Employee Manager"}).render();
     console.log(logoText);
+    viewOptions();
 }
 init();
-// function start() {
-//     inquirer
-//         .prompt([
-//             {
-//                 name: "item",
-//                 type: "list",
-//                 message: "What would you like to do?",
-//                 choices: ['VIEW', 'ADD', 'UPDATE', 'EXIT']
-//             }
-//         ])
-//         .then(function(answer) {
-//             switch (answer.item) {
-//                 case 'VIEW': 
-//                     return viewDepartments();
+function viewOptions() {
+    inquirer
+        .prompt([
+            {
+                name: "item",
+                type: "list",
+                message: "What would you like to do?",
+                choices: ['View all Employees', 'View all Roles', 'View all Departments', 'Add New Employee', 'Add New Role', 'Add New Department', 'Update Employee', 'Exit']
+            }
+        ])
+        .then(function(answer) {
+            switch (answer.item) {
+                case 'View all Employees': 
+                    return viewAllEmployees();
                     
-//                 case 'ADD':
-//                     return addDepartments();
+                case 'View all Roles':
+                    return viewAllRoles();
 
-//                 case 'UPDATE':
-//                     return updateDepartments();
+                case 'View all Departments':
+                    return viewAllDepartments();
+                
+                case 'Add New Employee':
+                    return addNewEmployee();
+
+                case 'Add New Role':
+                    return addNewRole();
+
+                case 'Add New Department':
+                    return addNewDeparment();
+
+                case 'Update Employee':
+                    return updateEmployee();
                     
-//                 case 'EXIT':
-//                     return connection.end();
-//             }
+                case 'EXIT':
+                    return connection.end();
+            }
             
-//         })
-// };
+        })
+};
 
-// function viewDepartments(){
-//     connection.query("SELECT * FROM employees", function(err, res){
-//         if(err) throw err;
-//         console.table(res);
-//         connection.end();
-// }
+function viewAllEmployees(){
+    connection.query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;", function(err, res){
+        if(err) throw err;
+        console.table(res);
+        connection.end();
+})
+}
 
+function viewAllRoles(){
+    connection.query("SELECT role.id, role.title, role.salary FROM role ORDER by role.id", function(err, res){
+        if(err) throw err;
+        console.table(res);
+        connection.end();
+})
+}
+function viewAllDepartments(){
+    connection.query("SELECT department.id, department.name FROM department ORDER by department.id", function(err, res){
+        if(err) throw err;
+        console.table(res);
+        connection.end();
+})
+function addNewEmployee(){
+    connection.query("INSERT INTO employee.id, employee.first_name, employee.last_name, employee.role_id, employee.manager_id INTO employee", function(err, res){
+        if(err) throw err;
+        console.table(res);
+        connection.end();
+})
+}
+}
 // function addDeparments(){
     
 //     inquirer
@@ -130,4 +167,4 @@ init();
 //                 }
 //             )
 //         })
-// }
+// 
---
+++ db/connection.js
@@ -0,0 +1,17 @@
+const util = require('util');
+const mysql = require('mysql');
+
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "rootroot",
+    database: "employees"
+})
+
+connection.connect();
+
+connection.query = util.promisify(connection.query, console.log("connected"));
+
+module.exports = connection;
+
